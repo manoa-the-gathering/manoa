d3f78988d3801745b8107830e857a1e99d9ebb98
creightonchan <cj38@hawaii.edu>
2016-10-31 17:56:49 -1000

    manoa

---
+++ README.md
@@ -0,0 +1,9 @@
+## Landing-Page
+<img class = "ui centered image" src="./doc/landingpage.jpg">
+
+## Rules-Page
+<img class = "ui centered image" src="./doc/rulespage.jpg">
+
+## About-Page
+<img class = "ui centered image" src="./doc/about.jpg">
+
---
+++ app/imports/startup/client/router.js
@@ -0,0 +1,29 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+  name: 'Home_Page',
+  action() {
+    BlazeLayout.render('App_Body2', { main: 'Home_Page' });
+  },
+});
+
+FlowRouter.route('/rules', {
+  name: 'Rules_Page',
+  action() {
+    BlazeLayout.render('App_Body2', { main: 'Rules_Page' });
+  },
+});
+
+FlowRouter.route('/about', {
+  name: 'About_Page',
+  action() {
+    BlazeLayout.render('App_Body2', { main: 'About_Page' });
+  },
+});
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
+  },
+};
