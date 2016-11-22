3f548a89e914fab152856f7de92fd27a6f688407
Lexa <acvmf@hawaii.edu>
2016-11-22 11:51:56 -1000

    Fix formatting on rules, add styles

--- app/imports/ui/pages/rules-page.js
+++ app/imports/ui/pages/rules-page.js
@@ -1,7 +1,11 @@
 import { Template } from 'meteor/templating';
 
+
 Template.Rules_Page.onRendered(function () {
   $('body').addClass('rulesbg');
+  $('.ui.accordion')
+   .accordion()
+  ;
 });
 
 Template.Rules_Page.onDestroyed(function () {
