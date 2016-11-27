a6227d6066a03e0ece5be1d3cb493f8543668fe7
JHeadland <jth9@hawaii.edu>
2016-11-27 11:54:24 -1000

    CAS Authentication works with anyone

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -5,7 +5,7 @@ import { _ } from 'meteor/underscore';
 /* eslint-disable no-console */
 
 
-/* Validate username, sending a specific error message on failure. */
+/* Validate username, sending a specific error message on failure.
 Accounts.validateNewUser(function (user) {
   if (user) {
     const username = user.services.cas.id;
@@ -15,6 +15,7 @@ Accounts.validateNewUser(function (user) {
   }
   throw new Meteor.Error(403, 'User not in the allowed list');
 });
+*/
 
 
 if (!Meteor.settings.cas) {
