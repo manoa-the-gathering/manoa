d8687a7bc36fb30925e2790f6e3f5f4ceef9e1d9
Your Name <ifang@hawaii.edu>
2016-12-14 12:06:12 -1000

    added deck class

---
+++ app/imports/api/battle/deck.js
@@ -0,0 +1,39 @@
+import {Random} from 'meteor/random';
+
+export class Deck {
+
+    constructor(cards) {
+        this.cards = cards; //an array of cards
+    }
+
+    deal() {
+
+        const hand = this.cards.slice(this.cards.length-7, this.cards.length);
+        this.cards = this.cards.slice(0, this.cards.length-7);
+       // console.log(hand);
+        // console.log(this.cards);
+        return hand;
+    }
+
+    shuffle() {
+
+        for (let i = this.cards.length - 1; i > 0; i--) {
+
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
+
+        }
+
+        //console.log(this.cards);
+    }
+
+    draw() {
+        card = this.cards.pop()
+        //console.log(card);
+        this.cards.pop()
+        //console.log(this.cards);
+        return card;
+    }
+}
--- app/imports/ui/pages/battle-page.js
+++ app/imports/ui/pages/battle-page.js
@@ -3,8 +3,17 @@ import { Meteor } from 'meteor/meteor';
 import { Session } from 'meteor/session';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Messages } from '../../api/msgs/msgs.js';
+import { Deck } from '../../api/battle/deck.js';
 
 let green = 20;
+let blue = 20;
+
+const nayaCards = ["arid mesa", "mountain", "lightning bolt", "grim lavamancer", "wild nacatl", "eidolon of the great revel", "boros charm", "atarka's command"];
+
+const NayaBurn = new Deck(nayaCards);
+NayaBurn.shuffle();
+NayaBurn.deal();
+NayaBurn.draw();
 
 Template.Battle_Page.events({
   'submit .new-message'(event) {
@@ -22,6 +31,21 @@ Template.Battle_Page.events({
     Session.set("green", green);
     console.log(green);
   },
+  'click .greenminus': function() {
+    green -= 1;
+    Session.set("green", green);
+    console.log(green);
+  },
+  'click .blueplus': function() {
+    blue += 1;
+    Session.set("blue", blue);
+    console.log(blue);
+  },
+  'click .blueminus': function() {
+    blue -= 1;
+    Session.set("blue", blue);
+    console.log(blue);
+  },
 });
 
 function scrollToBottom() {
@@ -37,12 +61,17 @@ Template.Battle_Page.helpers({
   },
   greenTotal() {
     return Session.get("green");
+  },
+  blueTotal() {
+    return Session.get("blue");
   }
 });
 
 
 Template.Battle_Page.onRendered(function () {
   Session.set("green", 20);
+  Session.set("blue", 20);
+
   $('body').addClass('battlebg');
 });
 
