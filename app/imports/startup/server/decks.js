a1e5b260b19a904f6b3e3ca39849e0423381b003
creightonchan <cj38@hawaii.edu>
2017-04-04 22:28:47 -1000

    preload default deck

--- app/imports/startup/server/decks.js
+++ app/imports/startup/server/decks.js
@@ -49,7 +49,7 @@ export const draws = new Mongo.Collection('draws');
 // });
 
 // Naya Burn Fill
-const nayaBurnCards = [
+const allCards = [
   {
     card: 'Arid Mesa',
     type: 'land',
@@ -429,21 +429,22 @@ Meteor.publish('pHand', function (userId) {
 
 Meteor.methods({
   'newGame'(userId) {
+    // Load default deck if none
     Meteor.users.update({ _id: userId, deck: { $exists: false } },
       {
         $set: {
-          deck: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '59'],
+          deck: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14',
+          '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
+          '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42',
+          '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56',
+          '57', '58', '59', '60'],
         },
       });
-    // for (let card of userId.deck) {
-    //   results.insert(nayaBurnCards[card]);
-    // }
     let player = Meteor.users.findOne({ _id: userId });
     player = JSON.parse(JSON.stringify(player.deck));
-    console.log(player);
     for (let i = 0; i < player.length; i++) {
       let index = player[i];
-      results.insert(nayaBurnCards[index]);
+      results.insert(allCards[index]);
     }
     // userId.deck.forEach(function (x) {
     //   results.insert(nayaBurnCards[x]);
